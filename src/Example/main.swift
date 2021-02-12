f18d06c0a2cd4ba8a39c14bdff1e9a15396a8931
Darren Mulholland <dmulholl@tcd.ie>
2021-02-12 16:00:41 +0000

    Housekeeping

--- src/Example/main.swift
+++ src/Example/main.swift
@@ -1,12 +1,17 @@
 import TermUtils
 
 let term = Terminal()
-print("[Hit Ctrl-C to exit]")
+print("[Type 'quit' or hit Ctrl-C to exit]")
 
 while true {
     do {
         let input = try term.getLine(prompt: ">>> ", color: .red)
-        print("\n" + input)
+        print()
+        if input == "quit" {
+            break
+        }
+        term.addHistoryItem(input)
+        print(input)
     } catch TermUtilsError.eof, TermUtilsError.ctrl_c {
         print()
         break
